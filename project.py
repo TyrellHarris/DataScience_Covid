087163a8e85170f646a85f63140dd2df0178b65e
ChristianOkokhere <69167323+ChristianOkokhere@users.noreply.github.com>
2020-08-05 19:35:41 -0500

    nav bar some graph movement

--- project.py
+++ project.py
@@ -7,5 +7,13 @@ app = Flask(__name__, static_url_path='', static_folder='')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/page2')
+def page2():
+    return app.send_static_file('page2.html')
+
+@app.route('/page3')
+def page3():
+    return app.send_static_file('page3.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
